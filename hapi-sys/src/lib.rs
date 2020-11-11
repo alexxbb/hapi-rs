be9391b2cd5353174231a9dd3ace6f70ca7de8bf
Alex Rusev <hou.alexx@gmail.com>
2020-11-10 20:15:05 -0800

    Deleted hapi-sys crate

--- hapi-sys/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod ffi {
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-    #![allow(dead_code)]
-    #![allow(clippy::all)]
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
