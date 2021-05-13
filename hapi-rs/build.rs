535744b3d96999f4eb6c4bfd631b60984f52bf49
alexxbb <hou.alexx@gmail.com>
2021-05-12 22:32:20 -0700

    Builds on Windows

--- hapi-rs/build.rs
+++ hapi-rs/build.rs
@@ -2,14 +2,20 @@ use std::path::Path;
 
 fn main() {
     let hfs = std::env::var("HFS").expect("HFS variable not set");
-    println!("cargo:rustc-link-lib=dylib=HAPIL");
+    let filename;
+    let lib_dir;
     if cfg!(target_os = "macos") {
-        let lib_dir = Path::new(&hfs).parent().unwrap().join("Libraries");
-        println!(
-            "cargo:rustc-link-search=native={}",
-            lib_dir.to_string_lossy()
-        );
-    } else {
-        println!("cargo:rustc-link-search=native={}/dsolib", hfs);
+        filename = "HAPIL";
+        let _lib_dir = Path::new(&hfs).parent().unwrap().join("Libraries");
+        lib_dir = _lib_dir.to_string_lossy().to_string();
+    } else if cfg!(target_os = "windows") {
+        filename = "libHAPIL";
+        lib_dir = format!("{}/custom/houdini/dsolib", hfs);
     }
+    else {
+        filename = "HAPIL";
+        lib_dir = format!("{}/dsolib", hfs);
+    }
+    println!("cargo:rustc-link-search=native={}", lib_dir);
+    println!("cargo:rustc-link-lib=dylib={}", filename);
 }
