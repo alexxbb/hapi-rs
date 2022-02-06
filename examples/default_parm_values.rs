885678cd28f8277aa09add819ea22aa60f5cf555
alexxbb <hou.alexx@gmail.com>
2022-02-05 18:31:59 -0800

    Add default asset parm example

---
+++ examples/default_parm_values.rs
@@ -0,0 +1,19 @@
+/// Prints hda parameters default values and types
+use hapi_rs::session::quick_session;
+use hapi_rs::Result;
+
+fn main() -> Result<()> {
+    let ses = quick_session()?;
+    let lib = ses.load_asset_file("otls/sesi/SideFX_spaceship.hda")?;
+    let parms = lib.get_asset_parms("SideFX::Object/spaceship")?;
+    for p in &parms {
+        println!(
+            "Parm {} - {:?} - {:?}",
+            p.name()?,
+            p.parm_type(),
+            p.default_value()
+        );
+    }
+
+    Ok(())
+}
