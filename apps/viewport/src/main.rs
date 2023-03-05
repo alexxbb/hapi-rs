b26b888ee71631d3b96a1f95c4095724d41dc6b8
Aleksei Rusev <hou.alexx@gmail.com>
2023-03-04 22:48:48 -0800

    Upload new vertices on parameter changed

--- apps/viewport/src/main.rs
+++ apps/viewport/src/main.rs
@@ -5,26 +5,27 @@ use eframe::egui::{Context, Key, Modifiers, Sense};
 use eframe::epaint::Vertex;
 use eframe::{egui, CreationContext, Frame};
 use egui::mutex::Mutex;
+use egui_glow::CallbackFn;
+use glow::HasContext;
 use std::ops::BitXorAssign;
 use std::sync::Arc;
 use ultraviolet as uv;
 
 struct ViewportApp {
     full_screen: bool,
-    meshes: Arc<Mutex<Vec<Shapes>>>,
+    mesh: Arc<Mutex<Mesh>>,
     draw_index: usize,
+    scale: f32,
 }
 
 impl ViewportApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let gl = cc.gl.as_ref().expect("Could not init gl Context");
+        let gl = cc.gl.as_ref().expect("Could not init gl Context").clone();
         Self {
             full_screen: false,
-            meshes: Arc::new(Mutex::new(vec![
-                Shapes::Square(Mesh::square(gl)),
-                Shapes::Triangle(Mesh::triangle(gl)),
-            ])),
+            mesh: Arc::new(Mutex::new(Mesh::triangle(gl, 1.0))),
             draw_index: 0,
+            scale: 1.0,
         }
     }
 }
@@ -48,21 +49,25 @@ impl eframe::App for ViewportApp {
                         ui.selectable_value(&mut self.draw_index, 0, "Square");
                         ui.selectable_value(&mut self.draw_index, 1, "Tri");
                     });
+                if ui
+                    .add(egui::Slider::new(&mut self.scale, 0.1..=1.0).text("Scale"))
+                    .changed()
+                {
+                    let mesh = self.mesh.lock();
+                    let mut vertices = &mut [-0.5f32, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0];
+                    vertices.iter_mut().for_each(|v| *v *= self.scale);
+                    mesh.upload(vertices);
+                }
             });
         egui::CentralPanel::default().show(ctx, |ui| {
             egui::Frame::canvas(ui.style()).show(ui, |ui| {
                 let resp = ui.allocate_rect(ui.max_rect(), Sense::click());
 
-                let meshes = Arc::clone(&self.meshes);
-                let draw_index = self.draw_index;
+                let mesh = Arc::clone(&self.mesh);
                 let callback = egui::PaintCallback {
                     rect: ui.max_rect(),
-                    callback: Arc::new(egui_glow::CallbackFn::new(move |info, painter| {
-                        let mesh = &meshes.lock()[draw_index];
-                        match mesh {
-                            Shapes::Triangle(m) => m.paint(painter.gl()),
-                            Shapes::Square(m) => m.paint(painter.gl()),
-                        }
+                    callback: Arc::new(CallbackFn::new(move |info, painter| {
+                        mesh.lock().paint(painter.gl());
                     })),
                 };
                 ui.painter().add(callback);
@@ -151,25 +156,29 @@ unsafe fn compile_gl_program(gl: &glow::Context) -> glow::Program {
 }
 
 struct Mesh {
+    gl: Arc<glow::Context>,
     program: glow::Program,
     vao: glow::VertexArray,
+    vbo: glow::Buffer,
+    ebo: glow::Buffer,
     num_vtx: i32,
 }
 
 impl Mesh {
-    fn triangle(gl: &glow::Context) -> Self {
+    fn triangle(gl: Arc<glow::Context>, scale: f32) -> Self {
         #[rustfmt::skip]
-        let vertices = &[
+        let mut vertices = &mut [
             -0.5f32, -0.5, 0.0,
             0.5, -0.5, 0.0,
             0.5, 0.5, 0.0
         ];
+        vertices.iter_mut().for_each(|v| *v *= scale);
         let indices = &[0, 1, 2];
 
         Mesh::new(gl, vertices.as_slice(), indices.as_slice())
     }
 
-    fn square(gl: &glow::Context) -> Self {
+    fn square(gl: Arc<glow::Context>) -> Self {
         #[rustfmt::skip]
         let vertices = &[
             -0.5, -0.5, 0.0,
@@ -182,26 +191,37 @@ impl Mesh {
         Mesh::new(gl, vertices.as_slice(), indices.as_slice())
     }
 
-    fn new(gl: &glow::Context, vertices: &[f32], indices: &[i32]) -> Self {
+    fn upload(&self, vertices: &[f32]) {
+        unsafe {
+            self.gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.vbo));
+            self.gl.buffer_data_u8_slice(
+                glow::ARRAY_BUFFER,
+                cast_slice(vertices),
+                glow::DYNAMIC_DRAW,
+            );
+            self.gl.bind_buffer(glow::ARRAY_BUFFER, None);
+        }
+    }
+
+    fn new(gl: Arc<glow::Context>, vertices: &[f32], indices: &[i32]) -> Self {
         use glow::HasContext as _;
 
         unsafe {
-            let program = compile_gl_program(gl);
+            let program = compile_gl_program(&gl);
 
             // Create Vertex Array Object. This is the object that describes what and how to
             // draw. Think of it as a preset.
             let vao = gl.create_vertex_array().expect("vertex array");
-            // Make it current
-            gl.bind_vertex_array(Some(vao));
-
             // Generate buffers
             let vbo = gl.create_buffer().expect("buffer");
             let ebo = gl.create_buffer().expect("ebo buffer");
+            // Make it current
+            gl.bind_vertex_array(Some(vao));
 
             // Bind VBO
             gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
             // Copy data to it
-            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, cast_slice(vertices), glow::STATIC_DRAW);
+            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, cast_slice(vertices), glow::DYNAMIC_DRAW);
 
             // Bind EBO
             gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
@@ -209,7 +229,7 @@ impl Mesh {
             gl.buffer_data_u8_slice(
                 glow::ELEMENT_ARRAY_BUFFER,
                 cast_slice(indices),
-                glow::STATIC_DRAW,
+                glow::DYNAMIC_DRAW,
             );
 
             // Create attribute pointer at location(0) in shader
@@ -219,8 +239,11 @@ impl Mesh {
             gl.enable_vertex_attrib_array(0);
 
             Self {
+                gl,
                 program,
                 vao,
+                vbo,
+                ebo,
                 num_vtx: vertices.len() as i32,
             }
         }
