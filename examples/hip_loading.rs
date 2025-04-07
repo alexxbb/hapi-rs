766cf6d84892b480cfadb294a6ffd25a1cd0a0fe
Aleksei Rusev <arusev@nvidia.com>
2025-04-07 10:35:03 -0700

    Add examples/hip_loading.rs

---
+++ examples/hip_loading.rs
@@ -0,0 +1,55 @@
+use hapi_rs::session::*;
+use std::thread::sleep;
+use std::time::Duration;
+use tempfile::NamedTempFile;
+
+/// Demonstrates how to save and load Hip files.
+fn main() -> Result<()> {
+    let sess = quick_session(Some(&SessionOptions::builder().threaded(true).build()))?;
+
+    println!("Generating scene");
+    for _ in 0..10 {
+        // create some nodes
+        sess.create_node("Object/simplebiped")?;
+    }
+
+    let hip_file = NamedTempFile::with_suffix(".hip")?;
+    println!("Saving scene to {}", hip_file.path().display());
+    sess.save_hip(hip_file.path(), false)?;
+
+    println!("Cleaning up");
+    sess.cleanup()?;
+    sess.initialize()?;
+
+    println!("Loading scene {}", hip_file.path().display());
+    sess.load_hip(hip_file.path(), true)?;
+
+    // Because the server is in threaded mode, we much poll the session for status.
+    loop {
+        sleep(Duration::from_millis(100));
+        let state = sess.get_cook_state_status()?;
+        match state {
+            SessionState::StartingLoad => {
+                println!("Starting loading");
+            }
+            SessionState::Loading => {
+                println!("Loading...")
+            }
+            SessionState::Cooking => {
+                println!("Cooking assets...")
+            }
+            SessionState::Ready => {
+                println!("Scene loaded!");
+                break;
+            }
+            SessionState::ReadyWithFatalErrors => {
+                let error = sess.get_cook_result_string(StatusVerbosity::Statusverbosity2)?;
+                eprintln!("Fatal errors when loading hip: {error}");
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
