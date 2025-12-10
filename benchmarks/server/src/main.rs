15477a4dd83c72d3f370550a482d3558ab1712e4
arusev <arusev@nvidia.com>
2025-12-10 10:41:11 -0800

    Update benchmark tool

--- benchmarks/server/src/main.rs
+++ benchmarks/server/src/main.rs
@@ -11,7 +11,8 @@ use hapi_rs::geometry::AttributeInfo;
 use hapi_rs::node::Geometry;
 use hapi_rs::parameter::{FloatParameter, Parameter};
 use hapi_rs::raw::ThriftSharedMemoryBufferType;
-use hapi_rs::server::ServerOptions;
+use hapi_rs::server::ThriftTransport;
+use hapi_rs::server::{ServerOptions, ThriftSharedMemoryTransportBuilder};
 use hapi_rs::session::{new_in_process_session, new_thrift_session, SessionOptions};
 use hapi_rs::Result;
 use nanorand::{Rng, WyRand};
@@ -33,8 +34,8 @@ const N_RUN: usize = 10;
 fn copy_geo(source: &Geometry, input_geo: &Geometry) -> Result<()> {
     let part = source.part_info(0)?;
     input_geo.set_part_info(&part)?;
-    let position_attr = source.get_position_attribute(part.part_id())?;
-    let positions_data = position_attr.get(0)?;
+    let position_attr = source.get_position_attribute(&part)?;
+    let positions_data = position_attr.unwrap().get(0)?;
     let vertex_list = source.vertex_list(&part)?;
     let face_counts = source.get_face_counts(&part)?;
     input_geo.set_vertex_list(0, &vertex_list)?;
@@ -52,7 +53,7 @@ fn copy_geo(source: &Geometry, input_geo: &Geometry) -> Result<()> {
     let payload_c_strings: Vec<&CStr> = payload_data.iter_cstr().collect();
 
     let color_data = source
-        .get_point_color_attribute(&part)?
+        .get_color_attribute(&part, AttributeOwner::Point)?
         .expect("Cd attribute")
         .get(part.part_id())?;
     input_geo
@@ -72,7 +73,7 @@ fn copy_geo(source: &Geometry, input_geo: &Geometry) -> Result<()> {
 fn main() -> Result<()> {
     let mut args = std::env::args();
     let session_options = SessionOptions::default().threaded(false);
-    let conn_name = "hapi-bench";
+    let conn_name = format!("hapi-bench-{}", random_string::<3>());
     let server_type = args.nth(1).expect("server type");
     let num_runs: usize = match args.next() {
         None => N_RUN,
@@ -80,21 +81,26 @@ fn main() -> Result<()> {
     };
     println!("Starting \"{server_type}\" server");
     let server_options = match server_type.as_str() {
-        "pipe" => ServerOptions::pipe(conn_name),
-        "memory" => ServerOptions::shared_memory()
-            .memory_name(conn_name)
-            .configure_thrift(|opts| {
-                opts.set_shared_memory_buffer_type(ThriftSharedMemoryBufferType::Buffer);
-                opts.set_shared_memory_buffer_size(1000);
-            }),
-        "in-process" => ServerOptions::shared_memory(), // not used
-        _ => panic!("Server type must be pipe or memory"),
+        "pipe" => ServerOptions::pipe_with_defaults(),
+        "memory-fixed" | "memory-ring" => {
+            let buffer_type = match server_type.as_str() {
+                "memory-fixed" => ThriftSharedMemoryBufferType::Buffer,
+                "memory-ring" => ThriftSharedMemoryBufferType::RingBuffer,
+                _ => unreachable!(),
+            };
+            ServerOptions::shared_memory_with_defaults()
+                .with_thrift_transport(ThriftTransport::SharedMemory(
+                    ThriftSharedMemoryTransportBuilder::default()
+                        .with_buffer_type(buffer_type)
+                        .with_memory_name(conn_name)
+                        .with_buffer_size(1) // MB
+                        .build(),
+                ))
+                .with_auto_close(false)
+        }
+        _ => panic!("Server type must be pipe or memory-fixed or memory-ring"),
     };
-    let session = match server_type.as_str() {
-        "pipe" | "memory" => new_thrift_session(session_options, server_options)?,
-        "in-process" => new_in_process_session(None)?,
-        _ => panic!("Server type must be one of pipe, memory, or in-process"),
-    };
+    let session = new_thrift_session(session_options, server_options)?;
     println!("Session created.");
     let asset_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("benchmark.hda");
     let lib = AssetLibrary::from_file(session.clone(), &asset_path)?;
@@ -110,18 +116,19 @@ fn main() -> Result<()> {
     let Ok(Parameter::String(input_str)) = asset_node.parameter("string") else {
         panic!("Missing string parameter");
     };
+    let mut total_time = Duration::from_millis(0);
     for n in 0..num_runs {
+        let start = std::time::Instant::now();
         rotate_parm.set(0, (n * 10) as f32);
         input_str.set(0, random_string::<10>())?;
         asset_node.cook()?;
         let geometry = asset_node.geometry()?.expect("geometry");
         let input = session.create_input_node(&format!("copy_{n}"), None)?;
-        copy_geo(&geometry, &input)?
+        copy_geo(&geometry, &input)?;
+        total_time += start.elapsed();
     }
-    println!(
-        "Server type \"{server_type}\" done in {} milliseconds",
-        start.elapsed().as_millis()
-    );
+    let avg_time = total_time.div_f32(num_runs as f32);
+    println!("Server type \"{server_type}\" done in {avg_time:.2?} milliseconds (avg)",);
     // session.save_hip("c:/Temp/junk/bench.hip", true)?;
     Ok(())
 }
