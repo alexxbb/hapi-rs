abeb2806288e930b7bf37a99f896414393c41435
arusev <arusev@nvidia.com>
2025-12-16 14:48:41 -0800

    Improve server type benchmark

--- benchmarks/server/src/main.rs
+++ benchmarks/server/src/main.rs
@@ -23,20 +23,22 @@ use std::sync::{LazyLock, Mutex};
 
 static RNG: LazyLock<Mutex<WyRand>> = LazyLock::new(|| Mutex::new(WyRand::new()));
 
-fn random_string<const SIZE: usize>() -> String {
+fn random_string(length: usize) -> String {
     let mut rng = RNG.lock().unwrap();
     let next = || rng.generate_range(98..122) as u8 as char;
-    std::iter::repeat_with(next).take(SIZE).collect()
+    std::iter::repeat_with(next).take(length).collect()
 }
 
-const NUM_ITERATIONS: usize = 3;
-const NUM_COPIES: usize = 20;
+const NUM_ITERATIONS: usize = 1;
+const NUM_COPIES: usize = 10;
 const WORKLOAD_MULTIPLIER: usize = 1;
+const STR_LENGTH: usize = 10;
 const BUFFER_SIZE: usize = 5_000; // GB
 
 fn copy_geo(source: &Geometry, input_geo: &Geometry) -> Result<()> {
     let part = source.part_info(0)?;
     input_geo.set_part_info(&part)?;
+    // dbg!(&part);
     let position_attr = source.get_position_attribute(&part)?;
     let positions_data = position_attr.unwrap().get(0)?;
     let vertex_list = source.vertex_list(&part)?;
@@ -72,8 +74,19 @@ fn copy_geo(source: &Geometry, input_geo: &Geometry) -> Result<()> {
     let dest_sdf_attr =
         input_geo.add_numeric_attribute::<f32>("sdf", part.part_id(), sdf_attr.info().clone())?;
     dest_sdf_attr.set(part.part_id(), &sdf_data)?;
+
+    let test_attr = input_geo.add_numeric_attribute::<f32>(
+        "test",
+        part.part_id(),
+        AttributeInfo::default()
+            .with_count(part.point_count())
+            .with_owner(AttributeOwner::Point)
+            .with_storage(StorageType::Float)
+            .with_tuple_size(1),
+    )?;
+    test_attr.set(part.part_id(), &[0.1, 0.2, 0.3])?;
     input_geo.commit()?;
-    input_geo.node.cook_blocking()?;
+    // input_geo.node.cook_blocking()?;
     // input_geo.save_to_file("/tmp/foo.bgeo")?;
 
     Ok(())
@@ -82,7 +95,7 @@ fn copy_geo(source: &Geometry, input_geo: &Geometry) -> Result<()> {
 fn main() -> Result<()> {
     let mut args = std::env::args();
     let session_options = SessionOptions::default().threaded(false);
-    let conn_name = format!("hapi-bench-{}", random_string::<3>());
+    let conn_name = format!("hapi-bench-{}", random_string(3));
     let server_type = args.nth(1).expect("server type");
     let workload_multiplier = match args.next() {
         None => WORKLOAD_MULTIPLIER,
@@ -93,6 +106,7 @@ fn main() -> Result<()> {
 
     let num_iterations = NUM_ITERATIONS * workload_multiplier;
     let num_copies = NUM_COPIES * workload_multiplier;
+    let str_length = STR_LENGTH * workload_multiplier;
 
     println!("Starting \"{server_type}\" server");
     let server_options = match server_type.as_str() {
@@ -122,7 +136,6 @@ fn main() -> Result<()> {
     let lib = AssetLibrary::from_file(session.clone(), &asset_path)?;
 
     let asset_node = lib.try_create_first()?;
-    println!("Test asset ready.");
     let start = std::time::Instant::now();
     let Ok(Parameter::Float(anim_param)) = asset_node.parameter("anim") else {
         panic!("Missing anim parameter");
@@ -135,23 +148,23 @@ fn main() -> Result<()> {
     if let Ok(Parameter::Int(num_copies_param)) = asset_node.parameter("num_copies") {
         num_copies_param.set(0, num_copies as i32)?;
     }
-    println!("Running {num_iterations} iterations");
+    println!("Running benchmark with {num_iterations} iterations, {num_copies} copies, {str_length} string length");
     let mut total_time = Duration::from_millis(0);
     for n in 0..num_iterations {
-        let start = std::time::Instant::now();
         anim_param.set(0, (n * 10) as f32);
-        point_string_param.set(0, random_string::<10>())?;
+        point_string_param.set(0, random_string(str_length))?;
         // TODO: substruct cooking time from total time?
         asset_node.cook()?;
         let geometry = asset_node.geometry()?.expect("geometry");
         let input = session.create_input_node(&format!("copy_{n}"), None)?;
+        let copy_start = std::time::Instant::now();
         copy_geo(&geometry, &input)?;
+        total_time += copy_start.elapsed();
         println!(
             " - iteration {}/{num_iterations} done in {:.2?} seconds",
             n + 1,
-            start.elapsed().as_secs_f32()
+            copy_start.elapsed().as_secs_f32()
         );
-        total_time += start.elapsed();
     }
     let avg_time = total_time.div_f32(num_iterations as f32).as_secs_f32();
     println!("Server type \"{server_type}\" done in {avg_time:.2} seconds (avg)",);
